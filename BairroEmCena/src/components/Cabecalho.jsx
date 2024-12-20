4701712cda6c4da1958b6810b6605d5af8ce63b0
Bruna Borges <bruna.lborges.matias@gmail.com>
2024-12-20 10:13:25 -0300

    boostrap no cabecalho

--- BairroEmCena/src/components/Cabecalho.jsx
+++ BairroEmCena/src/components/Cabecalho.jsx
@@ -1,16 +1,29 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import Logo from '../assets/images/Logo.png'
-function Cabecalho(){
-    return(
-        <>  
-            <div className="cabecalho">
-                <img src={Logo} alt="Logo-BairroEmCena" width="7%"/>
-                <nav className="navegacao">
-                    <a href="">Home</a>
-                    <a href="">Login</a>
-                    <a href="">Busca</a>
-                </nav>
-            </div>
-        </>
-    )
+
+function Cabecalho() {
+  return (
+    <Navbar expand="lg">
+      <Container >
+        <div>
+            <img src={Logo} alt="" className='logo'/>
+        </div>
+        <div>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#login">Login</Nav.Link>
+                <Nav.Link href="#busca">Busca</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+        </div>
+        
+      </Container>
+    </Navbar>
+  );
 }
-export default Cabecalho
+
+export default Cabecalho;
